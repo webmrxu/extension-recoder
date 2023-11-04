e2694faa6f251baf3d7cbfabd6374ff4ce3c7bfb
webmrxu <webmrxu@foxmail.com>
2023-11-04 16:39:34 +0800

    update

--- stock/src/getDaySecondLineData.js
+++ stock/src/getDaySecondLineData.js
@@ -3,32 +3,33 @@
  */
 const allClass = require('../data/allClass.json')
 const { getDaySecondLineData } = require('../apis/xueqiu');
-const { formatDate } = require('../utils/utils');
+const { formatDate, fileExists } = require('../utils/utils');
 const path = require('path');
 const fs = require('fs');
-// getDaySecondLineData().then(res => {
-//   const { data: { data} } = res;
-//   console.log(data);
-//   debugger
-// }).catch(console.log)
-
 
 async function getOneStock(index) {
-  let stock = allClass[index]
+  let stock = allClass[index];
+  // console.log(index, stock);
   if (stock && stock.length === 2) {
     let symbol = stock[0]
     let dateStr = formatDate();
     let directoryPath = path.join(__dirname, '../data/', dateStr);
     let filePath = path.join(__dirname, '../data/', dateStr, symbol + '.json');
+    if (fileExists(filePath)) {
+      console.log(index, ...stock, '已经存储');
+      setTimeout(() => {
+        // 递归获取下一股票
+        getOneStock(index+1);
+      }, 50);
+      return;
+    }
     try {
       let res = await getDaySecondLineData(symbol);
       const { data: { data } } = res;
-      
-      
       fs.mkdirSync(directoryPath, { recursive: true });
       let jsonStr = JSON.stringify(data);
       await fs.promises.writeFile(filePath, jsonStr);
-      console.log(...stock, '获取成功');
+      console.log(index+' ', ...stock, 'api数据获取成功');
       setTimeout(() => {
         // 递归获取下一股票
         getOneStock(index+1);
@@ -37,7 +38,12 @@ async function getOneStock(index) {
       console.log('获取分时数据异常', err);
     }
   } else {
+    if (index === allClass.length) {
+      console.log(index, '所有数据加载完成.');
+    } else {
+      console.log(index, '数据加载中断:', allClass.length - index, ' 条数据未加载');
+    }
     return;
   }
 }
-getOneStock(0)
+getOneStock(4940)
--- stock/utils/utils.js
+++ stock/utils/utils.js
@@ -1,12 +1,32 @@
-function formatDate(date = new Date()) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份从 0 开始，需加 1
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
+const fs = require('fs');
+const path = require('path');
 
-  return `${year}-${month}-${day}`;
+const utils = {
+  formatDate(date = new Date()) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份从 0 开始，需加 1
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+    return `${year}-${month}-${day}`;
+  },
+  // 检查任意路径下的文件是否存在
+  fileExists(filePath) {
+    try {
+      // 使用 fs.accessSync 来检查文件是否存在
+      fs.accessSync(filePath, fs.constants.F_OK);
+      return true; // 文件存在
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        return false; // 文件不存在
+      } else {
+        // 其他错误，如权限问题
+        throw err;
+      }
+    }
+  }
 }
-
-module.exports.formatDate = formatDate;
+// test
+module.exports = utils;
