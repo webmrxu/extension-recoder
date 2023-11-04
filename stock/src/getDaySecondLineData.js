2a07e99589fffaa30ff5dcc191d33eb16679656d
webmrxu <webmrxu@foxmail.com>
2023-11-04 12:52:10 +0800

    update

--- stock/src/getDaySecondLineData.js
+++ stock/src/getDaySecondLineData.js
@@ -1,11 +1,43 @@
 /**
  * 获取股票单日分时线
  */
-
+const allClass = require('../data/allClass.json')
 const { getDaySecondLineData } = require('../apis/xueqiu');
+const { formatDate } = require('../utils/utils');
+const path = require('path');
+const fs = require('fs');
+// getDaySecondLineData().then(res => {
+//   const { data: { data} } = res;
+//   console.log(data);
+//   debugger
+// }).catch(console.log)
 
-getDaySecondLineData().then(res => {
-  const { data: { data} } = res;
-  console.log(data);
-  debugger
-}).catch(console.log)
+
+async function getOneStock(index) {
+  let stock = allClass[index]
+  if (stock && stock.length === 2) {
+    let symbol = stock[0]
+    let dateStr = formatDate();
+    let directoryPath = path.join(__dirname, '../data/', dateStr);
+    let filePath = path.join(__dirname, '../data/', dateStr, symbol + '.json');
+    try {
+      let res = await getDaySecondLineData(symbol);
+      const { data: { data } } = res;
+      
+      
+      fs.mkdirSync(directoryPath, { recursive: true });
+      let jsonStr = JSON.stringify(data);
+      await fs.promises.writeFile(filePath, jsonStr);
+      console.log(...stock, '获取成功');
+      setTimeout(() => {
+        // 递归获取下一股票
+        getOneStock(index+1);
+      }, 1000);
+    } catch (err) {
+      console.log('获取分时数据异常', err);
+    }
+  } else {
+    return;
+  }
+}
+getOneStock(0)
---
+++ stock/utils/utils.js
@@ -0,0 +1,12 @@
+function formatDate(date = new Date()) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份从 0 开始，需加 1
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
+module.exports.formatDate = formatDate;
